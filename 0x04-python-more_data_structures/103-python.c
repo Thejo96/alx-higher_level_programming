546fff3cfc787ec15ef6f44ab7565f5e8ae7fe03
Thejo96 <thejopodcast2023@gmail.com>
2023-09-13 15:17:15 +0100

    Updated

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -61,9 +61,10 @@ void print_python_list(PyObject *p)
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
-{
+	{
 		obj = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
-}
+	}
+
