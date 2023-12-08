d6f1d5de14d02a50e116db937fd8d1e1dd4cd859
Thejo96 <thejopodcast2023@gmail.com>
2023-12-08 12:40:20 +0100

    Deleted

--- 4-never_empty.sql
+++
@@ -1,6 +0,0 @@
--- Creates a table with a constraint on a column and another
--- column that is never empty when given a NULL value
-CREATE TABLE IF NOT EXISTS id_not_null(
-    id INT DEFAULT 1,
-    name VARCHAR(256) NOT NULL
-);
